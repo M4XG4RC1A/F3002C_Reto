d216e0fc122be105ee088da94ef5211d8dda61d9
M4XG4RC1A <maxgarciae.c@hotmail.com>
2023-10-01 17:31:43 -0600

    Fase2 Files Add

--- Fase2/GetModesTable.m
+++ Fase2/GetModesTable.m
@@ -4,7 +4,7 @@ clc;
 
 Xs = linspace(825,1000,5);
 Ys = linspace(775,1000,5);
-Ls = linspace(1530,1580,3);
+Ls = ["\lambda", "A_{eff}"];
 
 lenX = length(Xs);
 lenY = length(Ys);
@@ -15,7 +15,7 @@ fileID = fopen("Table.txt",'w');
 %Start table
 fprintf(fileID,'%s\n',"\begin{table}[H]\centering");
 fprintf(fileID,'%s\n',"\resizebox{0.45\textwidth}{!}{");
-fprintf(fileID,'%s\n',"  \begin{tabular}{rr|"+convertCharsToStrings(repmat('c',1,lenX))+"|ll}");
+fprintf(fileID,'%s\n',"  \begin{tabular}{rr|"+convertCharsToStrings(repmat('c',1,lenX))+"|l}");
 fprintf(fileID,'%s\n',"     & &\multicolumn{"+num2str(lenX)+"}{c}{Wg_x}\\");
 
 strLine = "     & ";
@@ -26,17 +26,23 @@ strLine = strLine+"\\ \cline{1-"+num2str(lenX+4)+"} \\[-1em]";
 fprintf(fileID,'%s\n',strLine);
 
 firstStrStart = "     \multirow{"+num2str(lenL*lenY)+"}{*}{Wg_y}";
-endStrLine = "&\multirow{"+num2str(lenL*lenY)+"}{*}{$\lambda$}";
+endStrLine = "";%"&\multirow{"+num2str(lenL*lenY)+"}{*}{$\lambda$}";
 
 nmodes = 0;
 for y = Ys
     strLine = firstStrStart+"&\multirow{"+num2str(lenL)+"}{*}{"+num2str(y)+"nm}";
     for l = Ls
         for x=Xs
-            %load("./Modes/Waveguide"+string(x)+"_"+string(y)+"_"+string(l)+".mat");
-            strLine = strLine+"&"+num2str(nmodes);
+            load("./nModes/Waveguide"+string(x*1000)+"_"+string(y*1000)+".mat");
+            if l == "\lambda"
+                strLine = strLine+"&"+num2str(nmodes)+"("+num2str(x*y)+")";
+            else
+                line = sprintf('%.0f,' , nonzeros(Aeff));
+                line  = line(1:end-1);
+                strLine = strLine+"&"+line;
+            end
         end
-        strLine = strLine+"&"+num2str(l)+"nm"+endStrLine+"\\";
+        strLine = strLine+"&"+l+"nm"+endStrLine+"\\";
         fprintf(fileID,'%s\n',strLine);
         strLine = "&";
         endStrLine="";
